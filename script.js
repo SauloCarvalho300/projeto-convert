a1ec8b34026c5fe710f31fedb861b9a3f1bae48d
Saulo Carvalho <saulo_carvalho123@hotmail.com>
2024-07-25 20:24:22 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 //Manipulando o input para receber somente números.
 amount.addEventListener("input", () => {
@@ -39,17 +40,34 @@ function convertCurrency(amount, price, symbol) {
   try {
     //Exibindo a cotação da moeda selecionada.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    //Calcula o total.
+    let total = amount * price
+
+    //verifica se o resultado não é um número.
+    if (isNaN(total))
+      return alert("Por favor, digite o valor corretamente para converter.")
+
+    //Formata o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    //Exibe o resultado total.
+    result.textContent = `${total} Reais`
+
     //Aplica a classe quye exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
   } catch (error) {
+    //Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
     console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.")
   }
 }
-  //Formata a moeda em Real brasileiro.
+//Formata a moeda em Real brasileiro.
 function formatCurrencyBRL(value) {
   //Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00.00).
   return Number(value).toLocaleString("pt-BR", {
-    style:"currency",
+    style: "currency",
     currency: "BRL"
   })
 
